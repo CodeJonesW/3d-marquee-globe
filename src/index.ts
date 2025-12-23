88985999545d0664864b915b2ef64712e4ba59bf
Will Jones <williamjonescodes@gmail.com>
2025-12-22 19:07:07 -0600

    fix type declarations and scripts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { LedMarqueeOrbContainer } from './components/LedMarqueeOrbContainer'
 export type { LedMarqueeOrbContainerProps } from './components/LedMarqueeOrbContainer'
 export type { LedMarqueeOrbProps } from './components/LedMarqueeOrb'
+
